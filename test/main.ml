e9d48117ee46f60a46d7df54ab5e0c995e4c0192
Arjun Shah <ans96@cornell.edu>
2021-10-16 18:45:38 -0400

    added some dig tests

--- test/main.ml
+++ test/main.ml
@@ -3,13 +3,15 @@ open Game
 open Square
 
 (** square configuations *)
-let unmarked_mine = create_square true 1
+let mine = create_square true 1
 
-let marked_mine = unmarked_mine |> flag
+let marked_mine = mine |> flag
 
-let unmarked_square = create_square false 2
+let square = create_square false 2
 
-let marked_square = unmarked_mine |> flag
+let marked_square = mine |> flag
+
+let dug_square = square |> dig
 
 let create_square_test
     (name : string)
@@ -19,12 +21,17 @@ let create_square_test
   name >:: fun _ ->
   assert_equal expected_output (create_square is_mine around |> get_val)
 
-let flag_test
+let square_op_test
+    (op : Square.t -> Square.t)
+    (test : Square.t -> bool)
     (name : string)
     (square : Square.t)
     (expected_output : bool) : test =
-  name >:: fun _ ->
-  assert_equal expected_output (flag square |> get_mark)
+  name >:: fun _ -> assert_equal expected_output (op square |> test)
+
+let flag_test = square_op_test flag get_flag
+
+let dig_test = square_op_test dig get_dug
 
 let square_tests =
   [
@@ -44,14 +51,18 @@ let square_tests =
       false 10 None;
     flag_test "blank square should become marked when flagged" blank
       true;
-    flag_test "unmarked square should become marked when flagged"
-      unmarked_square true;
+    flag_test "unmarked square should become marked when flagged" square
+      true;
     flag_test "marked square should become unmarked when flagged"
       marked_square false;
-    flag_test "unmarked mine should become marked when flagged"
-      unmarked_mine true;
+    flag_test "unmarked mine should become marked when flagged" mine
+      true;
     flag_test "marked mine should become unmarked when flagged"
       marked_mine false;
+    ( "should raise Explode if a mine is dug up" >:: fun _ ->
+      assert_raises Explode (fun () -> dig mine) );
+    dig_test "blank sqaure should become dug when dug" blank true;
+    dig_test "undug sqaure should become dug when dug" square true;
   ]
 
 let board_tests = []
