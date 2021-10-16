29282ec231845cbe0c154b1a06115d365c07045f
Max Pace <maxpace1@icloud.com>
2021-10-16 17:21:55 -0400

    Dune things fixed

---
+++ src/board.ml
@@ -0,0 +1,3 @@
+type t = Square.t Array.t
+
+type loc = int * int
--- src/board.mli
+++ src/board.mli
@@ -13,10 +13,12 @@ exception Mine
 val empty : t
 (** An empty board with no mines placed and no squares revealed *)
 
-val set_mines : t -> t
-(** Returns a board with 99 mines placed pseudorandomly throughout the
-    board. Requires: input does not have any mines already set or
-    squares revealed *)
+val set_mines : t -> loc -> t
+(** [set_mines board index] returns a board with 99 mines placed
+    pseudorandomly throughout the board. There will not be a mine at the
+    location denoted by [index]. Requires: input does not have any mines
+    already set or squares revealed, and [index] is a valid location for
+    [board] *)
 
 val flag : t -> loc -> t
 (** [flag board index] flips the flagged state of the square located at
