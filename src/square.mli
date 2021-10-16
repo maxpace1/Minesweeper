147584433870306d56c5a829229514f46cb55112
Max Pace <maxpace1@icloud.com>
2021-10-16 16:33:44 -0400

    Square specs

--- src/square.mli
+++ src/square.mli
@@ -3,24 +3,17 @@
 type t
 (** The abstract type of value representing a place on the gameboard. *)
 
-
 (* Operations *)
 
-val mark: t -> t
-(** Flips the status of the marked mine
-    I.e. a marked mine becomes unmarked, and an unmarked mine becomes marked. *)
+val flag : t -> t
+(** Flips the status of the flagged mine i.e. a flagged mine becomes
+    unflagged, and an unflagged mine becomes flagged. *)
 
+val dig : t -> t
+(** "Digs up" a square. Requires: the square has not been dug before *)
 
-val dig : t -> t 
-(** "Digs up" a square
-    This method can only be invoked once on a given square, safety checks within the game will make sure of that. *)
- 
 (* Accessing data *)
 
-
-val get_display : t -> int 
-
-(** Chooses image for display
-    This method tells the game what image should be displayed on the square.
-    This method should also be used for determining valid dig operations.
-    Returns the number of mines surrounding for if it isn't a mine (0-8), or undug up (-1) or a marked mine (100) If exploded, return 1000 *)
+val get_val : t -> int
+(** Gets the number of surrounding mines of a square and returns an
+    integer 0-8 *)
