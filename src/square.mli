b6fb26810566d28a4b075b8f7bc92505b3e43a04
Jerry Xu <jjx6@cornell.edu>
2021-10-16 16:55:07 -0400

    Improved spec for square.mli

--- src/square.mli
+++ src/square.mli
@@ -3,17 +3,38 @@
 type t
 (** The abstract type of value representing a place on the gameboard. *)
 
+exception AlreadyDugUp
+(** Raised when attempting to dig up square that has already been dug up *)
+
 (* Operations *)
 
+val blank : t
+
+val create_square : bool -> int -> t
+(** Creates a square. The square is a mine if the first *)
+
 val flag : t -> t
-(** Flips the status of the flagged mine i.e. a flagged mine becomes
-    unflagged, and an unflagged mine becomes flagged. *)
+(** [flag sq] flips the status of the flagged square i.e. a flagged [sq]
+    becomes unflagged, and an unflagged [sq] becomes flagged. *)
 
 val dig : t -> t
-(** "Digs up" a square. Requires: the square has not been dug before *)
+(** [dig sq] "digs up" a square. Requires: [sq] has not been dug before *)
 
 (* Accessing data *)
 
-val get_val : t -> int
-(** Gets the number of surrounding mines of a square and returns an
-    integer 0-8 *)
+val get_dug : t -> bool
+(** [get_dug sq] checks if [sq] has been dug up yet. *)
+
+val get_mark : t -> bool
+(** [get_mark sq] checks if [sq] has been dug up yet. *)
+
+val get_explode : t -> bool
+(** [get_explode sq] returns [True] if [sq] was a mine and got dug up,
+    exploding and ending the game. *)
+
+val get_val : t -> int option
+(** [get_val sq] gets the number of surrounding mines of [sq] and
+    returns an integer 0-8 if it isn't a mine and has been dug up
+
+    it isn't a mine (0-8), or undug up (-1) or a marked mine (100) If
+    exploded, return 1000*)
