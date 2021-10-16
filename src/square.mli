d97cc25f5d82d03932f1f96e8637a11693c5ad43
Jerry Xu <jjx6@cornell.edu>
2021-10-16 17:20:56 -0400

    First implementation of Comp Unit square

--- src/square.mli
+++ src/square.mli
@@ -6,21 +6,24 @@ type t
 exception AlreadyDugUp
 (** Raised when attempting to dig up square that has already been dug up *)
 
+exception Explode
+
 (* Operations *)
 
 val blank : t
 
 val create_square : bool -> int -> t
-(** [flag is_mine around] creates a square. [is_mine] represents if the
-    square itself is a mine, and [around] represents the number of
-    squares around it that are mines.*)
+(** [create_square mine around] creates a square. [is_mine] represents
+    if the square itself is a mine, and [around] represents the number
+    of squares around it that are mines.*)
 
 val flag : t -> t
 (** [flag sq] flips the status of the flagged square i.e. a flagged [sq]
     becomes unflagged, and an unflagged [sq] becomes flagged. *)
 
 val dig : t -> t
-(** [dig sq] "digs up" a square. Requires: [sq] has not been dug before *)
+(** [dig sq] "digs up" a square. Raises [Explode] if square was a mine.
+    Requires: [sq] has not been dug before, or will raise [AlreadyDugUp] *)
 
 (* Accessing data *)
 
@@ -30,13 +33,7 @@ val get_dug : t -> bool
 val get_mark : t -> bool
 (** [get_mark sq] checks if [sq] has been dug up yet. *)
 
-val get_explode : t -> bool
-(** [get_explode sq] returns [true] if [sq] was a mine and got dug up,
-    exploding and ending the game. *)
-
 val get_val : t -> int option
 (** [get_val sq] gets the number of surrounding mines of [sq] and
-    returns an integer 0-8 if it isn't a mine and has been dug up
-
-    it isn't a mine (0-8), or undug up (-1) or a marked mine (100) If
-    exploded, return 1000*)
+    returns an integer 0-8 if it isn't a mine, or if [sq] has not been
+    dug up yet, returns [None] *)
